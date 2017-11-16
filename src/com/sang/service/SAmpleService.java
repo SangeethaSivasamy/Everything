e5acfe7201d29924e75154479951770e602a4797
SangeethaSivasamy <sangeetha.sps@gmail.com>
2017-11-16 13:07:50 -0800

    commit 2
    commit 2

--- src/com/sang/service/SAmpleService.java
+++ src/com/sang/service/SAmpleService.java
@@ -8,7 +8,7 @@ import com.sang.dao.SampleDAO;
 public class SAmpleService {
 	Logger logger = LogManager.getLogger(SAmpleService.class);
 public String getName() {
-	logger.info("inside SAmpleService");
+	logger.info("inside SAmpleService..");
 	SampleDAO sampleDAO = new SampleDAO();
 	return sampleDAO.getName();
 }
